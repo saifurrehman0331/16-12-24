91476a0a3a2aef1854fd4a3b3f52f9086412e74e
Saif Ur Rehman <bonvages6@gmail.com>
2024-12-17 00:07:16 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// alert("Error! please enter a valid password");
+// alert("Welcome to JS Land \n happy coding!");
+alert("Welome to JS land");
+alert("Happy Coding");
+
+
+// let user ="john \n";
+// let age= "25\n ";
+// let message ='hello \n';
+// alert(user+ age+ message);
+let age="my age is 15 year old" ;
+alert(age);
+
+    
+
+
+
+
+
+
+
+document.write("hello buddy");
